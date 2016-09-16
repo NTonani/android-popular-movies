780b4dc3f8de64d0838d1e3ab808ebe65ad6f557
Nathan Tonani <nathantonani@gmail.com>
2016-09-15 19:29:53 -0700

    Handled main activity recreation problem by clearing backstack

--- app/src/main/java/com/nathantonani/popularmovies/DetailActivity.java
+++ app/src/main/java/com/nathantonani/popularmovies/DetailActivity.java
@@ -1,8 +1,11 @@
 package com.nathantonani.popularmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 
 /**
  * Created by ntonani on 9/12/16.
@@ -11,10 +14,21 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             Log.v("DetailActivity", " OnCreate -- Starting fragment");
             getSupportFragmentManager().beginTransaction().add(R.id.container,new MovieDetailsFragment()).commit();
         }
     }
+
+    @Override
+    public Intent getParentActivityIntent() {
+        Intent intent = super.getParentActivityIntent();
+        if (intent != null) {
+            return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        }
+        return intent;
+    }
+
 }
--- app/src/main/java/com/nathantonani/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/nathantonani/popularmovies/SettingsActivity.java
@@ -4,19 +4,21 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.widget.Toolbar;
 
 /**
  * Created by ntonani on 9/12/16.
  */
 public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener{
 
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.pref_general);
-
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_sort_key)));
+
+
 
     }
 
